c585ed93d648a812d235d68e4d2e0112890cee32
Jasmine <jayjadesanders@gmail.com>
2025-04-07 12:58:05 -0400

    Completed Part 4: The Spread Operator

--- script.js
+++ script.js
@@ -45,6 +45,7 @@ studentToJSON = JSON.parse(studentToJSON)
 console.log(studentToJSON)
 console.log(`\n`)
 
+
 // Part 3: Using Destructuring Assignment
 // Use destructuring to extract the name and courses properties from the student object.
 console.log("Part 3: Using Destructuring Assignment")
@@ -59,3 +60,31 @@ let [score1, score2] = scores
 console.log("Destructured scores")
 console.log("score 1:", score1)
 console.log("score 2:", score2)
+console.log(`\n`)
+
+
+// Part 4: The Spread Operator
+console.log("Part 4: The Spread Operator")
+
+// Clone student object
+let clonedObj = {...student}
+console.log("Cloned student Object:", clonedObj)
+console.log(`\n`)
+
+// Add a new property (e.g., graduationYear) to the cloned object and log it.
+console.log("Create graduation Year property")
+clonedObj.graduationYear = 2026
+console.log("Graduation Year:", clonedObj.graduationYear)
+console.log("cloned student object:", clonedObj)
+console.log(`\n`)
+
+// Merge two arrays:
+let newCourses = [
+    "Python",
+    "Java",
+    "React"
+]
+
+student.courses = [...student.courses, ...newCourses]
+console.log("Combined new courses to student.courses")
+console.log("Student.courses:", student.courses)
